3a97d31b250e21698a2a84e9e7f8384a73707548
Maria Sanli <marisanli.93@gmail.com>
2023-04-23 22:47:02 -0400

    connected editted files

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Calculator");
+
+
+const button1 = document.getElementById("button-1");
+button1.addEventListener("click", function() {
+  const currentValue = document.getElementById("calculator-display").innerHTML;
+  const newValue = currentValue + "1";
+  document.getElementById("calculator-display").innerHTML = newValue;
+});
+
+
+console.log('Calculator');
